917049f73530ea898f3fd28db88f3e008cc0afd3
kwabber7 <angeliraf1@gmail.com>
2026-01-12 10:03:04 +0100

    main: update

--- docs/uitwerking_GIP/fase_2_ontwikkeling_databank/test_queries.sql
+++ docs/uitwerking_GIP/fase_2_ontwikkeling_databank/test_queries.sql
@@ -5,12 +5,30 @@ JOIN tasks ON users.id = tasks.user_id;
 
 -- UPDATE
 UPDATE tasks
+SET title = 'nieuwe title'
+WHERE id = 1;
+
+UPDATE tasks
+SET description = 'beschrijving van taak'
+WHERE id = 1;
+
+UPDATE tasks
+SET due_date = '2026-01-24'
+WHERE id = 1;
+
+UPDATE tasks
 SET status = 'done'
 WHERE id = 1;
 
 --login
+SELECT id, username
+FROM users
+WHERE email = 'jan@email.com'
+AND password = 'hashedpassword1';
 
-
+--aanmelding
+INSERT INTO users (username, password, email)
+VALUES ('nieuwenaam', 'gehasht_wachtwoord', 'nieuw@email.com');
 
 -- DELETE
 DELETE FROM tasks
