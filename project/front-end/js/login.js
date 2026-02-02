48e23495117c6a24a021923bdaea6238cbd92a06
amine.kiadi <aminekiadi@hotmail.com>
2026-02-02 11:15:34 +0100

    main: login en register

--- project/front-end/js/login.js
+++ project/front-end/js/login.js
@@ -1,12 +1,33 @@
 const loginButton = document.querySelector('#loginBtn')
 const loginOverlay = document.querySelector('.overlay-login')
-const closeOverlay = document.querySelector('#closeBtn')
+const closeLogin = document.querySelector('#closeBtnLogin')
+const registerLink = document.querySelector('#registerFormLink')
+const registerOverlay = document.querySelector('.overlay-register')
+const closeRegister = document.querySelector('#closeBtnRegister')
+const loginLink = document.querySelector('#loginFormLink')
+
 
 
 loginButton.addEventListener("click", function(){
     loginOverlay.style.display = "flex"
 });
 
-closeOverlay.addEventListener("click", function(){
+closeLogin.addEventListener("click", function(){
     loginOverlay.style.display = "none"
 });
+
+registerLink.addEventListener("click", function(){
+    loginOverlay.style.display = "none"
+    registerOverlay.style.display = "flex"
+});
+
+closeRegister.addEventListener("click", function(){
+    registerOverlay.style.display = "none"
+});
+
+loginLink.addEventListener("click", function(){
+    registerOverlay.style.display = "none"
+    loginOverlay.style.display = "flex"
+});
+
+
