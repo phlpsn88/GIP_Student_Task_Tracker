06469f5caa2fd64f5239812fec6f97c8eeac481b
amine.kiadi <aminekiadi@hotmail.com>
2026-02-12 16:03:58 +0100

    main: rafje

--- project/front-end/js/popup.js
+++ project/front-end/js/popup.js
@@ -1,10 +1,18 @@
 const newTaskButton = document.querySelector('.btn-new-task');
 const newTaskOverlay = document.querySelector('.overlay-new-task')
 const closeNewTask = document.querySelector('#closeNewTask')
+const datePicker = document.querySelector('#datePicker')
+
 
 newTaskButton.addEventListener("click", function(){
     newTaskOverlay.style.display = "flex"
     document.body.classList.add("remove-scrolling");
+
+    try{
+        datePicker.showPicker();
+    } catch(error) {
+        console.log(error)
+    }
 });
 
 
