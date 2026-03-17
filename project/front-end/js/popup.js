0f7a5aaec3f7e7967e8a8851cc551557d16662b8
Wout Philipsen <phlpsn@Wouts-MacBook-Air.local>
2026-03-17 14:14:28 +0100

    main: task edit

--- project/front-end/js/popup.js
+++ project/front-end/js/popup.js
@@ -3,35 +3,38 @@ const newTaskOverlay = document.querySelector('.overlay-new-task')
 const closeNewTask = document.querySelector('#closeNewTask')
 const datePicker = document.querySelector('#datePicker')
 const editTaskButton = document.querySelector('.btn-edit-task');
-const pencilEdit = document.querySelector('.pencil-edit-task')
 const editTaskOverlay = document.querySelector('#edit-task-overlay')
 const closeEditTask = document.querySelector('#closeEditTask')
 
-newTaskButton.addEventListener("click", function(){
+newTaskButton.addEventListener("click", function () {
     newTaskOverlay.style.display = "flex"
     document.body.classList.add("remove-scrolling");
 
-    try{
+    try {
         datePicker.showPicker();
-    } catch(error) {
+    } catch (error) {
         console.log(error)
     }
 });
 
 
-closeNewTask.addEventListener("click", function(){
+closeNewTask.addEventListener("click", function () {
     newTaskOverlay.style.display = "none"
     document.body.classList.remove("remove-scrolling");
 });
 
 
-
-pencilEdit.addEventListener("click", function(){
-    editTaskOverlay.style.display = "flex"
-       document.body.classList.remove("remove-scrolling");
-});
-
-closeEditTask.addEventListener("click", function(){
+closeEditTask.addEventListener("click", function () {
     editTaskOverlay.style.display = "none"
     document.body.classList.remove("remove-scrolling");
+});
+
+
+const iconsEdit = document.querySelectorAll("#pencil-edit-task");
+
+iconsEdit.forEach(edit => {
+    edit.addEventListener("click", function () {
+        editTaskOverlay.style.display = "flex"
+        document.body.classList.remove("remove-scrolling");
+    });
 });
