0f3cb9d030482f92f9c8e6d42fca05ed26d526b6
kwabber7 <angeliraf1@gmail.com>
2026-03-19 10:30:50 +0100

    main: website gewerkt

--- project/fixit.md
+++ project/fixit.md
@@ -4,4 +4,4 @@ login.js:44 Uncaught TypeError: Cannot read properties of null (reading 'addEven
     at login.js:44:16
 
 (anonymous) @ login.js:44Understand this error
-task_maneger.html:1 Unchecked runtime.lastError: A listener indicated an asynchronous response by returning true, but the message channel closed before a response was received
+task_manager.html:1 Unchecked runtime.lastError: A listener indicated an asynchronous response by returning true, but the message channel closed before a response was received
--- project/front-end/js/popup.js
+++ project/front-end/js/popup.js
@@ -5,6 +5,9 @@ const datePicker = document.querySelector('#datePicker')
 const editTaskButton = document.querySelector('.btn-edit-task');
 const editTaskOverlay = document.querySelector('#edit-task-overlay')
 const closeEditTask = document.querySelector('#closeEditTask')
+const deleteButtons = document.querySelectorAll(".btn-delete");
+const deletePopup = document.getElementById("popup");
+
 
 newTaskButton.addEventListener("click", function () {
     newTaskOverlay.style.display = "flex"
@@ -38,3 +41,15 @@ iconsEdit.forEach(edit => {
         document.body.classList.remove("remove-scrolling");
     });
 });
+
+deleteButtons.forEach(btn => {
+    btn.addEventListener("click", function () {
+        deletePopup.style.display = "flex";
+        document.body.classList.add("remove-scrolling");
+    });
+});
+
+function closePopup() {
+    deletePopup.style.display = "none";
+    document.body.classList.remove("remove-scrolling");
+}
