a2d387b28f6d6f24e8e301ef199710b9b3841b01
kwabber7 <angeliraf1@gmail.com>
2026-04-21 22:33:27 +0200

    main: website is af front end alle fouten opgelost en responcive

--- Client/front-end/js/popup.js
+++ Client/front-end/js/popup.js
@@ -1,7 +1,6 @@
 const newTaskButton = document.querySelector('.btn-new-task');
 const newTaskOverlay = document.querySelector('.overlay-new-task')
 const closeNewTask = document.querySelector('#closeNewTask')
-const datePicker = document.querySelector('#datePicker')
 const editTaskButton = document.querySelector('.btn-edit-task');
 const editTaskOverlay = document.querySelector('#edit-task-overlay')
 const closeEditTask = document.querySelector('#closeEditTask')
@@ -12,12 +11,6 @@ const deletePopup = document.getElementById("popup");
 newTaskButton.addEventListener("click", function () {
     newTaskOverlay.style.display = "flex"
     document.body.classList.add("remove-scrolling");
-
-    try {
-        datePicker.showPicker();
-    } catch (error) {
-        console.log(error)
-    }
 });
 
 
@@ -26,6 +19,9 @@ closeNewTask.addEventListener("click", function () {
     document.body.classList.remove("remove-scrolling");
 });
 
+editTaskButton.addEventListener("click", function () {
+    document.body.classList.add("remove-scrolling");
+});
 
 closeEditTask.addEventListener("click", function () {
     editTaskOverlay.style.display = "none"
